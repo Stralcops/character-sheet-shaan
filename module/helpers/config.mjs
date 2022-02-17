4343682e8e80a35464e2295ba56d0f4c2b2e8da6
Stralcops <stralcops@icloud.com>
2022-02-17 19:06:38 +0100

    Ajout foundry

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const SHAAN = {};
+
+// Define constants here, such as:
+SHAAN.foobar = {
+  'bas': 'SHAAN.bas',
+  'bar': 'SHAAN.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SHAAN.abilities = {
+  "str": "SHAAN.AbilityStr",
+  "dex": "SHAAN.AbilityDex",
+  "con": "SHAAN.AbilityCon",
+  "int": "SHAAN.AbilityInt",
+  "wis": "SHAAN.AbilityWis",
+  "cha": "SHAAN.AbilityCha"
+};
+
+SHAAN.abilityAbbreviations = {
+  "str": "SHAAN.AbilityStrAbbr",
+  "dex": "SHAAN.AbilityDexAbbr",
+  "con": "SHAAN.AbilityConAbbr",
+  "int": "SHAAN.AbilityIntAbbr",
+  "wis": "SHAAN.AbilityWisAbbr",
+  "cha": "SHAAN.AbilityChaAbbr"
+};
